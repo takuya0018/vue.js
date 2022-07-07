a237c2165c95d8c9726161bdabb4b9c2d7eec47d
きくたたくや <gtk.9223@gmail.com>
2022-07-07 19:07:10 +0900

    ６まで終わった

---
+++ 3/3-1/js/main.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            text: "hello vue.js"
+        }
+    },
+    methods: {
+        handleClick(){
+            this.text = "button pushed"
+        }
+    }
+})
---
+++ 5/5-1/js/main.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            items: ["りんご","ばなな","ぶどう","いちご","パイナップル"],
+            obj: {
+                name:"takuya",
+                age:35,
+                like:"sushi"
+            }
+        }
+    }
+})
